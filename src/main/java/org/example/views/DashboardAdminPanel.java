0f7fc2b283cfcbfdea860b6123518010f611a64f
= <=>
2025-07-04 14:49:53 +0700

    14:49 - 4/7

--- src/main/java/org/example/views/DashboardAdminPanel.java
+++ src/main/java/org/example/views/DashboardAdminPanel.java
@@ -3,7 +3,6 @@ package org.example.views;
 import com.formdev.flatlaf.FlatLightLaf;
 import org.example.entity.Room;
 import org.example.service.RoomService;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
--- src/main/java/org/example/views/RegisterFinishPanel.java
+++ src/main/java/org/example/views/RegisterFinishPanel.java
@@ -1,36 +1,5 @@
-//package org.example.views;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class RegisterFinishPanel extends JPanel {
-//    private MainFrame mainFrame;
-//
-//    private JLabel lblMessage;
-//    private JButton btnLogin;
-//
-//    public RegisterFinishPanel(MainFrame mainFrame) {
-//        this.mainFrame = mainFrame;
-//        setLayout(null);
-//        initComponents();
-//    }
-//
-//    private void initComponents() {
-//        lblMessage = new JLabel("Đăng ký thành công!", SwingConstants.CENTER);
-//        lblMessage.setFont(new Font("Arial", Font.BOLD, 22));
-//        lblMessage.setBounds(400, 200, 350, 40);
-//        add(lblMessage);
-//
-//        btnLogin = new JButton("Đăng nhập ngay");
-//        btnLogin.setBounds(475, 260, 200, 35);
-//        add(btnLogin);
-//
-//        btnLogin.addActionListener(e -> mainFrame.showLoginPanel());
-//    }
-//}
 
 package org.example.views;
-
 import javax.swing.*;
 import java.awt.*;
 
